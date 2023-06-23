be64f24b012e684430158257bb7c8f58fac67289
dannycahyo <dannydwicahyono@gmail.com>
2023-06-24 06:48:24 +0700

    feat: init and setup starter pack of the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import 'src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+  DehydratedState,
+} from "@tanstack/react-query";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "src/styles/globals.css";
+
+import type { AppProps } from "next/app";
+
+export type MainProps = {
+  dehydratedState: DehydratedState;
+};
+
+export default function App({ Component, pageProps }: AppProps<MainProps>) {
+  const { dehydratedState } = pageProps;
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
