6ceba989ac3fce429c0d7652ca929ebd13d1d9c2
dannycahyo <dannydwicahyono@gmail.com>
2023-06-24 22:30:39 +0700

    chore: Configure Storybook and Daisy UI for project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { Container } from "src/uikits";
+
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      <h1 className="text-4xl font-bold">Welcome to home page!</h1>
-    </main>
+    <Container>
+      <div className="prose">
+        <h1>Welcome to home page!</h1>
+        <h2>Next.js + TypeScript + Tailwind CSS + daisyUI</h2>
+      </div>
+      <button className="btn btn-primary">Hello daisyUI</button>
+    </Container>
   );
 }
