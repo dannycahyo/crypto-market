850d0ecf24709611804508bf71e085bb7644b3b5
dannycahyo <dannydwicahyono@gmail.com>
2023-06-25 09:54:17 +0700

    fix: Change img element into Next Image

--- src/market/components/TagCard.tsx
+++ src/market/components/TagCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import type React from "react";
 
 type TagCardProps = {
@@ -8,8 +10,7 @@ type TagCardProps = {
 const TagCard: React.FC<TagCardProps> = ({ iconURL, title }) => {
   return (
     <div className="inline-flex  flex-row items-center justify-between rounded-lg bg-blue-200/[1] p-2 text-blue-500">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={iconURL} width={32} height={32} alt={title} />
+      <Image src={iconURL} width={32} height={32} alt={title} />
       <span className="ml-2 text-base text-blue-500">{title}</span>
     </div>
   );
