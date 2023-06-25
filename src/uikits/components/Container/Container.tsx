ed981f1f48ba8a836ac3d57e66c0759a36944cb7
dannycahyo <dannydwicahyono@gmail.com>
2023-06-25 09:53:13 +0700

    refactor: Change Tailwind styling into prettier format

--- src/uikits/components/Container/Container.tsx
+++ src/uikits/components/Container/Container.tsx
@@ -6,7 +6,7 @@ type ContainerProps = {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="bg-white p-6 min-h-screen container mx-auto">
+    <div className="container mx-auto min-h-screen bg-white p-6">
       <div>{children}</div>
     </div>
   );
--- src/uikits/components/Skeleton/Skeleton.tsx
+++ src/uikits/components/Skeleton/Skeleton.tsx
@@ -1,15 +1,15 @@
 const Skeleton = () => {
   return (
-    <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-      <div className="animate-pulse flex space-x-4">
+    <div className="mx-auto w-full max-w-sm rounded-md border border-gray-300 p-4 shadow">
+      <div className="flex animate-pulse space-x-4">
         <div className="flex-1 space-y-6 py-1">
-          <div className="h-2 bg-slate-700 rounded" />
+          <div className="h-2 rounded bg-slate-700" />
           <div className="space-y-3">
             <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-slate-700 rounded col-span-2" />
-              <div className="h-2 bg-slate-700 rounded col-span-1" />
+              <div className="col-span-2 h-2 rounded bg-slate-700" />
+              <div className="col-span-1 h-2 rounded bg-slate-700" />
             </div>
-            <div className="h-2 bg-slate-700 rounded" />
+            <div className="h-2 rounded bg-slate-700" />
           </div>
         </div>
       </div>
