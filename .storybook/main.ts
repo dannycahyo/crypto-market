bffa17d281dc856599536e42ba0df021ba4dfd45
dannycahyo <dannydwicahyono@gmail.com>
2023-06-27 15:36:13 +0700

    fix: Fix import alias in Storybook environment

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,27 +1,38 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+import type { StorybookConfig } from "@storybook/nextjs";
+
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-essentials',
-    '@storybook/addon-styling',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-essentials",
+    "@storybook/addon-styling",
     {
-      name: '@storybook/addon-styling',
+      name: "@storybook/addon-styling",
       options: {
         postcss: {
-          implementation: require.resolve('postcss'),
+          implementation: require.resolve("postcss"),
         },
       },
     },
   ],
   framework: {
-    name: '@storybook/nextjs',
+    name: "@storybook/nextjs",
     options: {},
   },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve = {
+      ...config.resolve,
+    };
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+
+    return config;
+  },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
 export default config;
