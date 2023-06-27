686f807d4e07e7c48bcc6f87c7849da22efbc1eb
dannycahyo <dannydwicahyono@gmail.com>
2023-06-27 10:58:00 +0700

    feat: Implement server side fetching on the market tag detail page

--- src/pages/market/tags/[slug]/index.tsx
+++ src/pages/market/tags/[slug]/index.tsx
@@ -1,3 +1,38 @@
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+
 import { MarketTagDetailScreen } from "src/market/screens";
+import {
+  getMarketTagDetail,
+  getPriceChanges,
+  getSupportedCurrencies,
+} from "src/market/fetcher";
+import { getQuery } from "src/utils";
+
+import type { MainProps } from "../../../_app";
+import type { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps<MainProps> = async ({
+  query,
+}) => {
+  const queryClient = new QueryClient();
+
+  const slug = getQuery(query.slug, "");
+
+  await queryClient.prefetchQuery(["supportedCurrencies"], () =>
+    getSupportedCurrencies()
+  );
+  await queryClient.prefetchQuery(["priceChanges"], () => getPriceChanges());
+
+  await queryClient.prefetchQuery(
+    ["marketTagDetail", { slug, language: "ID" }],
+    () => getMarketTagDetail({ slug, language: "ID" })
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
 
 export default MarketTagDetailScreen;
