e3fa7d91a7ef8bf5d54b40de7ef36718bf07c80e
dannycahyo <dannydwicahyono@gmail.com>
2023-06-29 18:06:58 +0700

    feat: Handle the TagListWidget component error case

--- src/market/widgets/TagListWidget/TagListWidget.tsx
+++ src/market/widgets/TagListWidget/TagListWidget.tsx
@@ -1,29 +1,33 @@
 import Link from "next/link";
 import { TagCard } from "src/market/components";
 import { useGetMarketTags } from "src/market/services";
-import { Skeleton } from "src/uikits";
+import { GeneralError, Skeleton } from "src/uikits";
 
 const TagListWidget = () => {
-  const { data: marketTags, isLoading: isLoadingMarketTags } = useGetMarketTags(
-    {
-      language: "ID",
-      sort: "ASC",
-    }
-  );
+  const {
+    data: marketTags,
+    isLoading: isLoadingMarketTags,
+    isError: isErrorMarketTags,
+  } = useGetMarketTags({
+    language: "ID",
+    sort: "ASC",
+  });
+
+  if (isLoadingMarketTags) return <Skeleton />;
+  if (isErrorMarketTags)
+    return (
+      <GeneralError message="There is something wrong when getting the tag list content. Please try again later." />
+    );
 
   return (
     <div className="mb-4 overflow-x-auto whitespace-nowrap">
-      {isLoadingMarketTags ? (
-        <Skeleton />
-      ) : (
-        <div className="flex gap-2">
-          {marketTags?.map((tag) => (
-            <Link href={`/market/tags/${tag.slug}`} key={tag.id}>
-              <TagCard title={tag.title} iconURL={tag.icon.url} />
-            </Link>
-          ))}
-        </div>
-      )}
+      <div className="flex gap-2">
+        {marketTags?.map((tag) => (
+          <Link href={`/market/tags/${tag.slug}`} key={tag.id}>
+            <TagCard title={tag.title} iconURL={tag.icon.url} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
