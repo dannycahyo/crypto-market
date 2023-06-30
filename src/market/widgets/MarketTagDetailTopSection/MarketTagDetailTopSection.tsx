b09d57855c081444b267b01bfada229b04a55c74
dannycahyo <dannydwicahyono@gmail.com>
2023-06-30 07:41:03 +0700

    feat: Handle the MarketTagDetailTopSection component error case

--- src/market/widgets/MarketTagDetailTopSection/MarketTagDetailTopSection.tsx
+++ src/market/widgets/MarketTagDetailTopSection/MarketTagDetailTopSection.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { useGetMarketTagDetail } from "src/market/services";
-import { Skeleton } from "src/uikits";
+import { GeneralError, Skeleton } from "src/uikits";
 
 import type React from "react";
 type MarketTagDetailTopSectionProps = {
@@ -12,39 +12,42 @@ type MarketTagDetailTopSectionProps = {
 const MarketTagDetailTopSection: React.FC<MarketTagDetailTopSectionProps> = ({
   slug,
 }) => {
-  const { data: marketTags, isLoading: isLoadingMarketTags } =
-    useGetMarketTagDetail({ language: "ID", slug });
+  const {
+    data: marketTags,
+    isLoading: isLoadingMarketTags,
+    isError: isErrorMarketTags,
+  } = useGetMarketTagDetail({ language: "ID", slug });
 
   const marketDetail = marketTags?.[0];
 
+  if (isLoadingMarketTags) return <Skeleton />;
+  if (isErrorMarketTags)
+    return (
+      <GeneralError message="There is something wrong when getting the market tag detail content. Please try again later." />
+    );
+
   return (
     <div>
-      {isLoadingMarketTags ? (
-        <Skeleton />
-      ) : (
-        <>
-          <div className="breadcrumbs text-sm">
-            <ul>
-              <li>
-                <Link href="/">Harga Crypto</Link>
-              </li>
-              <li>{`${marketDetail?.title} Category`}</li>
-            </ul>
-          </div>
-          <div className="mb-4 mt-6 flex items-center gap-4">
-            <Image
-              alt={marketDetail?.title ?? ""}
-              width={28}
-              height={28}
-              src={marketDetail?.icon.url ?? ""}
-            />
-            <h1 className="text-md text-black sm:text-2xl">
-              {marketDetail?.title}
-            </h1>
-          </div>
-          <p className="text-md mb-">{marketDetail?.subtitle}</p>
-        </>
-      )}
+      <div className="breadcrumbs text-sm">
+        <ul>
+          <li>
+            <Link href="/">Harga Crypto</Link>
+          </li>
+          <li>{`${marketDetail?.title} Category`}</li>
+        </ul>
+      </div>
+      <div className="mb-4 mt-6 flex items-center gap-4">
+        <Image
+          alt={marketDetail?.title ?? ""}
+          width={28}
+          height={28}
+          src={marketDetail?.icon.url ?? ""}
+        />
+        <h1 className="text-md text-black sm:text-2xl">
+          {marketDetail?.title}
+        </h1>
+      </div>
+      <p className="text-md mb-2">{marketDetail?.subtitle}</p>
     </div>
   );
 };
