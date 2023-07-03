51b77af0ef4a81998290bd7fffce9188297f7e82
dannycahyo <dannydwicahyono@gmail.com>
2023-07-03 12:21:08 +0700

    test: Implement sorting interaction test on the TokenListWidget component

--- src/market/widgets/TokenListWidget/TokenListWidget.stories.tsx
+++ src/market/widgets/TokenListWidget/TokenListWidget.stories.tsx
@@ -1,5 +1,9 @@
 import { expect } from "@storybook/jest";
-import { within, waitForElementToBeRemoved } from "@storybook/testing-library";
+import {
+  within,
+  waitForElementToBeRemoved,
+  userEvent,
+} from "@storybook/testing-library";
 
 import { TokenListWidget } from "./TokenListWidget";
 import { filterCurrenciesByMarketTagCurrency } from "src/market/utils";
@@ -78,8 +82,14 @@ TokenListWidgetByMarketTagError.parameters = {
 
 TokenListWidgetByMarketSuccess.play = async ({ canvasElement, step }) => {
   const canvas = within(canvasElement);
+
   await step("wait for loading to be finished", async () => {
-    await waitForElementToBeRemoved(() => canvas.queryAllByRole("progressbar"));
+    await waitForElementToBeRemoved(
+      () => canvas.queryAllByRole("progressbar"),
+      {
+        timeout: 30000,
+      }
+    );
   });
 
   for (const { name } of SupportedCurrenciesTestData) {
@@ -88,6 +98,119 @@ TokenListWidgetByMarketSuccess.play = async ({ canvasElement, step }) => {
       expect(heading).toBeInTheDocument();
     });
   }
+
+  const clickPriceColumnButton = async (name: string): Promise<void> => {
+    const button = await canvas.findByRole("button", { name });
+    userEvent.click(button);
+  };
+
+  const checkTokensOrder = async (
+    order: "asc" | "desc" | "unsorted",
+    defaultToken: (string | null)[]
+  ): Promise<void> => {
+    const tokenHeadings = await canvas.findAllByRole("heading", { level: 3 });
+    const tokenState = await Promise.all(
+      tokenHeadings
+        .map((heading: HTMLElement) => heading.textContent)
+        .filter((str) => str?.startsWith("Rp."))
+    );
+
+    const compareTokenPrices = (a: string, b: string) => {
+      const priceA = parseFloat(a.replace("Rp. ", "").replace(",", ""));
+      const priceB = parseFloat(b.replace("Rp. ", "").replace(",", ""));
+
+      if (priceA < priceB) {
+        return -1;
+      }
+      if (priceA > priceB) {
+        return 1;
+      }
+      return 0;
+    };
+
+    const ascendingTokenPrices = [...defaultToken].sort((a, b) => {
+      if (a && b) {
+        return compareTokenPrices(a, b);
+      }
+      return 0;
+    });
+
+    const descendingTokenPrices = [...defaultToken].sort((a, b) => {
+      if (a && b) {
+        return compareTokenPrices(b, a);
+      }
+      return 0;
+    });
+
+    if (order === "desc") {
+      tokenState.forEach((tokenPrice, index) => {
+        expect(tokenPrice).toEqual(descendingTokenPrices[index]);
+      });
+    } else if (order === "asc") {
+      tokenState.forEach((tokenPrice, index) => {
+        expect(tokenPrice).toEqual(ascendingTokenPrices[index]);
+      });
+    } else if (order === "unsorted") {
+      tokenState.forEach((tokenPrice, index) => {
+        expect(tokenPrice).toEqual(defaultToken[index]);
+      });
+    }
+  };
+
+  const tokenHeadings = await canvas.findAllByRole("heading", { level: 3 });
+  const defaultTokenPrices = tokenHeadings
+    .map((heading: HTMLElement) => heading.textContent)
+    .filter((str) => str?.startsWith("Rp."));
+
+  await step(
+    "click up icon button of latest price column section",
+    async () => {
+      await clickPriceColumnButton("descOrder Harga");
+    }
+  );
+
+  await step(
+    "check if tokens price are sorted by latest price in descending order",
+    async () => {
+      await checkTokensOrder("desc", defaultTokenPrices);
+    }
+  );
+
+  await step(
+    "click up icon button of latest price column section again",
+    async () => {
+      await clickPriceColumnButton("descOrder Harga");
+    }
+  );
+
+  await step("check if tokens are back to unsorted state", async () => {
+    await checkTokensOrder("unsorted", defaultTokenPrices);
+  });
+
+  await step(
+    "click bottom icon button of latest price column section",
+    async () => {
+      await clickPriceColumnButton("ascOrder Harga");
+    }
+  );
+
+  await step(
+    "check if tokens are sorted by latest price in ascending order",
+    async () => {
+      await checkTokensOrder("asc", defaultTokenPrices);
+    }
+  );
+
+  await step(
+    "click bottom icon button of latest price column section again",
+    async () => {
+      await clickPriceColumnButton("ascOrder Harga");
+    }
+  );
+
+  await step("check if tokens are back to unsorted state", async () => {
+    await checkTokensOrder("unsorted", defaultTokenPrices);
+  });
 };
 
 TokenListWidgetByMarketError.play = async ({ canvasElement, step }) => {
